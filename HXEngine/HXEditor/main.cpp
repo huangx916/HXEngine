ca13adfdebd54c66cffa40a65b927b415ea19668
Xin Huang <huangx916@163.com>
2018-08-02 13:26:34 +0800

    adjust scene

--- HXEngine/HXEditor/main.cpp
+++ HXEngine/HXEditor/main.cpp
@@ -1,9 +1,13 @@
 #include "HXEditorWin.h"
 #include <QApplication>
+//#include <QStyleFactory>
 //#include "vgl.h"
 
 int main(int argc, char *argv[])
 {
+	/*QStringList lt = QStyleFactory::keys();
+	QApplication::setStyle(QStyleFactory::create("Fusion"));
+	QApplication::setPalette(QApplication::style()->standardPalette());*/
 	QApplication a(argc, argv);
 	//glutInit(&argc, argv);
 	HXEditorWin::GetInstance()->show();
