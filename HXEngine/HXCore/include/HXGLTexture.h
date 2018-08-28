76548f662a213e409da06ca030622093d4ae7e0e
huangx916 <huangx916@163.com>
2018-08-29 07:55:55 +0800

    adjust texture class

--- HXEngine/HXCore/include/HXGLTexture.h
+++ HXEngine/HXCore/include/HXGLTexture.h
@@ -20,9 +20,23 @@ namespace HX3D
 		GLuint LoadDDSTexture(const char *filename);
 		GLuint LoadDDSTextureCubeMap(const char *filename);
 
-		// 
-		GLuint CreateFloat32Texture(int32_t width, int32_t height, bool enableMipmapping = true);
+		//----------------------------------------------------
+		void Create(const char* texture_name, bool enableMipmapping = true);
+		void CreatePrefilterCubeMap(const char* textureName);
+		void CreatePrefilterTableMap(const char* textureName);
+		void Create(int32_t width, int32_t height, bool enableMipmapping = true);
+		void CreateFloat32Texture(int32_t width, int32_t height, bool enableMipmapping = true);
+		void CreateFloat32DepthTexture(int32_t width, int32_t height, bool enableMipmapping = true);
+		void CreateFloat32CubeTexture(int32_t width, int32_t height, bool enableMipmapping = true);
+		void CreateFloat16CubeTexture(int32_t width, int32_t height, bool enableMipmapping = true);
+		void CreateFloat323DTexture(int32_t width, int32_t height, int32_t depth, bool enableMipmapping = true);
 
+	protected:
+		static void CreateGLTexture2D(int32_t tex_obj, int32_t width, int32_t height, int8_t* texture_data, int32_t texture_pixel_format, bool enableMipmapping);
+		static void CreateGLTextureCube(int32_t tex_obj, int32_t width, int32_t height, int8_t* texture_data, int32_t texture_pixel_format, bool enableMipmapping);
+		static void SetTexturePixelFormat(HXGLTexture* texture, int32_t texture_pixel_format);
+
+	public:
 		// 具体纹理数据保存在显存上了，系统内存不需要再保存一份
 		GLuint texId;
 	};
