1faa2a3ff91e4a5ac9f6fb9e4629cade3f0e4185
huangx916 <huangx916@163.com>
2019-06-08 20:29:05 +0800

    todo add meta file

--- HXEngine/HXCore/src/HXGLTexture.cpp
+++ HXEngine/HXCore/src/HXGLTexture.cpp
@@ -253,6 +253,8 @@ namespace HX3D
 				}
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+				/*glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);*/
 				glBindTexture(GL_TEXTURE_2D, 0);
 				break;
 			}
