f46e1002ea539cca1e7c50003e13f85b1f3f2505
huangx916 <huangx916@163.com>
2019-06-16 22:04:12 +0800

    wrap meta

--- HXEngine/HXCore/src/HXTextureMetaData.cpp
+++ HXEngine/HXCore/src/HXTextureMetaData.cpp
@@ -14,24 +14,41 @@ namespace HX3D
 
 	void HXTextureMetaData::ParseMetaFile(std::string strPath)
 	{
-		std::ifstream t(strPath);
-		if (!t.is_open())
+		std::ifstream i(strPath);
+		if (!i.is_open())
 		{
 			SerializeMetaFile(strPath);
 			return;
 		}
-		std::stringstream buffer;
-		buffer << t.rdbuf();
+		json j;
+		i >> j;
+
+		/*std::stringstream buffer;
+		buffer << i.rdbuf();
 		std::string contents(buffer.str());
-		auto j = json::parse(contents);
+		auto j = json::parse(contents);*/
+
 		version = j["version"].get<string>();
 		uuid = j["uuid"].get<string>();
 		wrapMode = (ETextureWrapMode)j["wrapMode"].get<int>();
 		filterMode = (ETextureFilterMode)j["filterMode"].get<int>();
+		i.close();
 	}
 
 	void HXTextureMetaData::SerializeMetaFile(std::string strPath)
 	{
+		json j;
+		j["version"] = version;
+		j["uuid"] = uuid;
+		j["wrapMode"] = wrapMode;
+		j["filterMode"] = filterMode;
 
+		std::ofstream o(strPath);
+		if (o.is_open())
+		{
+			o << j.dump(4) << endl;
+			o.flush();
+			o.close();
+		}
 	}
 }
